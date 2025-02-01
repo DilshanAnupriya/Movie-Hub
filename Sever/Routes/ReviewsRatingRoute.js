d6864e5cffcb0270dbf8b5eceef67fb3baaf2091
dilshan anupriya <dilshananupriya@icloud.com>
2025-02-02 02:18:24 +0530

    role define

--- Sever/Routes/ReviewsRatingRoute.js
+++ Sever/Routes/ReviewsRatingRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ReviewRatingController= require('../Controller/ReviewsRatingController');
+const verifyToken= require('../middleware/Auth');
 
-router.post('/create',ReviewRatingController.createReview);
-router.get('/get-all',ReviewRatingController.GetAllReview);
-router.get('/get-by/:id',ReviewRatingController.GetReviewById);
-router.put('/update/:id',ReviewRatingController.updateReview);
-router.delete('/delete/:id',ReviewRatingController.deleteReview);
+
+router.post('/create',verifyToken(['admin','user']),ReviewRatingController.createReview);
+router.get('/get-all',verifyToken(['admin','user']),ReviewRatingController.GetAllReview);
+router.get('/get-by/:id',verifyToken(['admin','user']),ReviewRatingController.GetReviewById);
+router.put('/update/:id',verifyToken(['admin']),ReviewRatingController.updateReview);
+router.delete('/delete/:id',verifyToken(['admin']),ReviewRatingController.deleteReview);
 
 module.exports = router;
--- Sever/Routes/SearchHistoryRoute.js
+++ Sever/Routes/SearchHistoryRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const SearchController= require('../Controller/SearchHistoryController');
-
-router.post('/log-search', SearchController.logSearch);
-router.get('/list',SearchController.trendingList);
+const verifyToken= require('../middleware/Auth');
+router.post('/log-search',verifyToken(['user']), SearchController.logSearch);
+router.get('/list',verifyToken(['admin','user']),SearchController.trendingList);
 
 module.exports = router;
 
